b813230e5237c4686b0c89689c4b0012d58e9c4e
Rishi SHeth <sheth19@purdue.edu>
2021-02-27 16:35:18 -0500

    first commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Rishi Sheth on 2/26/21.
+//  Copyright © 2021 Rishi Sheth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
