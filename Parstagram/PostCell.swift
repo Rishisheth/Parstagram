400c151a1ecf6fdf458eeac1ab9042a18f591502
Rishi SHeth <sheth19@purdue.edu>
2021-02-27 22:57:33 -0500

    yes

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Rishi Sheth on 2/27/21.
+//  Copyright © 2021 Rishi Sheth. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
